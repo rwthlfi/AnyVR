024293c732ad0edc7a9b1e3b3c69453705fc6021
Linus <linus.witte@proton.me>
2024-09-14 00:11:06 +0200

    init text chat

---
+++ Assets/Scripts/TextChat/TextChatManager.cs
@@ -0,0 +1,135 @@
+// AnyVR is a multiuser, multiplatform XR framework.
+// Copyright (C) 2024 Engineering Hydrology, RWTH Aachen University.
+// 
+// AnyVR is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+// 
+// AnyVR is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT-
+// ABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with AnyVR.
+// If not, see <https://www.gnu.org/licenses/>.
+
+using FishNet.Connection;
+using FishNet.Object;
+using System;
+using UnityEngine;
+
+namespace TextChat
+{
+    public class TextChatManager : NetworkBehaviour
+    {
+        #region Singleton
+
+        internal static TextChatManager s_instance;
+
+        private void InitSingleton()
+        {
+            if (s_instance != null)
+            {
+                Debug.LogWarning("Instance of TextChatManager already exists!");
+                Destroy(this);
+            }
+
+            s_instance = this;
+        }
+
+        #endregion
+        
+        
+        private readonly CircularBuffer<TextMessage> _buffer = new(100);
+
+        internal event Action<TextMessage> TextMessageReceived;
+
+        internal event Action MessagesSynced;
+
+        private void Awake()
+        {
+            InitSingleton();
+        }
+
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+            Debug.Log("Requesting Buffer");
+            RequestBuffer();
+        }
+
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+            _buffer.Push(new TextMessage(-1, "Start of chat"));
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        private void SendTextMessage(string msg, NetworkConnection sender)
+        {
+            TextMessage textMessage = new(sender.ClientId, msg);
+            _buffer.Push(textMessage);
+            ReceiveTextMessage(textMessage);
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        private void RequestBuffer(NetworkConnection conn = null)
+        {
+            if (conn == null)
+            {
+                return;
+            }
+
+            Debug.Log("Syncing Buffer");
+            TextMessage[] buffer = _buffer.GetAll();
+            for(int i = 0; i < buffer.Length; i++)
+            {
+                Debug.Log($"{i}'th element: {buffer[i]._message}");
+                
+            }
+            SyncBuffer(conn, buffer);
+        }
+
+        [TargetRpc]
+        private void SyncBuffer(NetworkConnection conn, TextMessage[] buffer)
+        {
+            _buffer.Clear();
+            foreach (TextMessage textMessage in buffer)
+            {
+                _buffer.Push(textMessage);
+            }
+            for(int i = 0; i < buffer.Length; i++)
+            {
+                Debug.Log($"{i}'th element: {buffer[i]._message}");
+            }
+            MessagesSynced?.Invoke();
+        }
+
+        [ObserversRpc]
+        private void ReceiveTextMessage(TextMessage msg)
+        {
+            _buffer.Push(msg);
+            TextMessageReceived?.Invoke(msg);
+        }
+
+        public CircularBuffer<TextMessage> GetBuffer()
+        {
+            return _buffer;
+        }
+    }
+    
+    public struct TextMessage
+    {
+        internal readonly int _senderId;
+        internal readonly string _message;
+
+        public TextMessage(int senderId, string message)
+        {
+            _senderId = senderId;
+            _message = message;
+        }
+    }
+
+}
