a0bfccadc3b0c588885b36b06597b5c88a871133
Linus <linus.witte@proton.me>
2024-10-24 14:05:27 +0200

    UIScene update

--- Assets/Scripts/LobbySystem/UI/LobbyRoom/PausePanelHandler.cs
+++ Assets/Scripts/LobbySystem/UI/LobbyRoom/PausePanelHandler.cs
@@ -4,37 +4,9 @@ namespace LobbySystem.UI.LobbyRoom
 {
     public class PausePanelHandler : MonoBehaviour
     {
-        [SerializeField] private GameObject _pausePanel;
-
-        private bool _pauseMenuActive;
-
-        private void Start()
-        {
-            _pauseMenuActive = gameObject.activeSelf;
-            if (_pauseMenuActive)
-            {
-                TogglePauseMenu();
-            }
-        }
-
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                TogglePauseMenu();
-            }
-        }
-
         public void Continue()
         {
-            _pauseMenuActive = false;
-            _pausePanel.gameObject.SetActive(_pauseMenuActive);
-        }
-
-        public void TogglePauseMenu()
-        {
-            _pauseMenuActive = !_pauseMenuActive;
-            _pausePanel.gameObject.SetActive(_pauseMenuActive);
+            UIHandler.s_instance.SetPanelActive(Panel.PausePanel, false);
         }
 
         public void QuitToMainMenu()
