bb842017de584d669c781db3ce19a43deff765ef
Linus Witte <linus.witte@proton.me>
2024-12-19 19:19:17 +0100

    Add runtime tests

---
+++ Tests/Runtime/ServerTests.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using Tests.Runtime;
+using UnityEngine.TestTools;
+
+namespace LobbySystem.LobbyTests
+{
+    public class ServerTests : LobbyTest
+    {
+        [UnityTest]
+        public IEnumerator StartServerTest()
+        {
+            // yield return StartServer();
+            // yield return JoinServer();
+            yield return null;
+        }
+    }
+}
