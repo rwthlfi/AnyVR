7732eb05a7e83e70e92ce36090a89e744120d280
Linus Witte <linus.witte@proton.me>
2024-12-23 16:11:23 +0100

    Add LobbySceneMetaData

---
+++ Editor/LobbySceneMetaData.cs
@@ -0,0 +1,18 @@
+using GameKit.Dependencies.Utilities.Types;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LobbySystem.UI
+{
+    [CreateAssetMenu(menuName = "AnyVr/LobbySceneMetaData")]
+    public class LobbySceneMetaData : ScriptableObject
+    {
+        [SerializeField] [Scene] private string _scene;
+        [SerializeField] [TextArea] private string _description;
+        [SerializeField] private Sprite _thumbnail;
+        public string Scene => _scene;
+
+        public string Description => _description;
+        public Sprite Thumbnail => _thumbnail;
+    }
+}
