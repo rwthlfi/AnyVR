ef30be57c41b5eb5fb346d878a03680bdad130c8
Linus Witte <linus.witte@proton.me>
2025-01-20 18:35:08 +0100

    Setup new WelcomeScene

---
+++ Samples~/NewLobbySetup/Scripts/WelcomeSceneHandler.cs
@@ -0,0 +1,29 @@
+using AnyVr.LobbySystem;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AnyVr.Samples.NewLobbySetup
+{
+    public class WelcomeSceneHandler : MonoBehaviour
+    {
+        [SerializeField] private TMP_InputField serverIpInputField;
+        [SerializeField] private TMP_InputField userNameInputField;
+        [SerializeField] private Button connectButton;
+        private ConnectionManager _connectionManager;
+
+        private void Start()
+        {
+            _connectionManager = ConnectionManager.GetInstance();
+            connectButton.onClick.AddListener(OnConnectBtnClicked);
+        }
+
+        private async void OnConnectBtnClicked()
+        {
+            string tokenServerIp = serverIpInputField.text;
+            ServerAddressResponse result = await ConnectionManager.RequestServerIp(tokenServerIp);
+            _connectionManager.ConnectToServer(result);
+            Debug.Log(result);
+        }
+    }
+}
