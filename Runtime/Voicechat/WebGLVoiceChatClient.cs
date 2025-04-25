ea95faf97366518040ce8f493bd8d297909f7f2c
Patrick Querl <patrick.querl@rwth-aachen.de>
2025-04-25 16:37:33 +0200

    Edit WebGLVoiceChatClient.cs

--- Runtime/Voicechat/WebGLVoiceChatClient.cs
+++ Runtime/Voicechat/WebGLVoiceChatClient.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-namespace AnyVr.Voicechat
+namespace AnyVR.Voicechat
 {
     internal class WebGLVoiceChatClient : VoiceChatClient
     {
