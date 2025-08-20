eb81bdd843fa05031540c09aca73f0c4eda8810c
Linus Witte <linus.witte@proton.me>
2025-08-20 18:27:18 +0200

    Add LobbyId and IsAdmin SyncVar in LobbyPlayerState

--- Runtime/LobbySystem/LobbyPlayerState.cs
+++ Runtime/LobbySystem/LobbyPlayerState.cs
@@ -1,6 +1,13 @@
+using System;
+using System.Linq;
+using AnyVR.Logging;
+using AnyVR.Voicechat;
 using FishNet.Connection;
 using FishNet.Object;
+using FishNet.Object.Synchronizing;
 using UnityEngine;
+using UnityEngine.Assertions;
+using Logger = AnyVR.Logging.Logger;
 
 namespace AnyVR.LobbySystem
 {
@@ -10,7 +17,10 @@ namespace AnyVR.LobbySystem
         ///     Prefab to spawn for the player.
         /// </summary>
         [SerializeField] private NetworkObject _playerPrefab;
-        
+
+        private readonly SyncVar<bool> _isAdmin = new(); // WritePermission is ServerOnly by default
+        private readonly SyncVar<Guid> _lobbyId = new(Guid.Empty);
+
         private NetworkObject _onlinePlayer;
 
         public override void OnStartServer()
@@ -19,10 +29,27 @@ namespace AnyVR.LobbySystem
 
             if (_playerPrefab == null)
                 return;
-            
+
+            LobbyHandler lobbyHandler = 
+                gameObject.scene.GetRootGameObjects()
+                    .Select(root => root.GetComponent<LobbyHandler>())
+                    .FirstOrDefault(comp => comp != null);
+
+            Assert.IsNotNull(lobbyHandler);
+
+            _lobbyId.Value = lobbyHandler.GetLobbyId();
+            _isAdmin.Value = lobbyHandler.MetaData.CreatorId == OwnerId;
+
             NetworkObject nob = Instantiate(_playerPrefab);
             _onlinePlayer = nob;
             Spawn(nob, Owner, gameObject.scene);
+        }
+
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+            Assert.IsFalse(_lobbyId.Value == Guid.Empty);
+            VoiceChatManager.GetInstance()?.TryConnectToRoom(_lobbyId.Value, GetName(), ConnectionManager.GetInstance()!.UseSecureProtocol);
         }
 
         public override void OnDespawnServer(NetworkConnection conn)
@@ -32,6 +59,52 @@ namespace AnyVR.LobbySystem
             {
                 Despawn(_onlinePlayer);
             }
+        }
+
+        public void PromoteToAdmin()
+        {
+            if (IsServerStarted)
+            {
+                PromoteToAdmin_Internal();
+            }
+            else if (!_isAdmin.Value)
+            {
+                ServerRPC_PromoteToAdmin(ClientManager.Connection);
+            }
+        }
+
+        [ServerRpc]
+        private void ServerRPC_PromoteToAdmin(NetworkConnection conn)
+        {
+            if (_isAdmin.Value)
+                return;
+
+            LobbyHandler lobbyHandler = LobbyHandler.GetInstance();
+            if (lobbyHandler == null)
+                return;
+
+            LobbyPlayerState caller = lobbyHandler.GetPlayerState<LobbyPlayerState>(conn.ClientId);
+            if (!caller.GetIsAdmin())
+                return;
+
+            PromoteToAdmin_Internal();
+        }
+
+        [Server]
+        private void PromoteToAdmin_Internal()
+        {
+            _isAdmin.Value = true;
+            Logger.Log(LogLevel.Verbose, nameof(LobbyPlayerState), $"Player {OwnerId} promoted to admin.");
+        }
+
+        public bool GetIsAdmin()
+        {
+            return _isAdmin.Value;
+        }
+        
+        public Guid GetLobby()
+        {
+            return _lobbyId.Value;
         }
     }
 }
--- Runtime/LobbySystem/PlayerState.cs
+++ Runtime/LobbySystem/PlayerState.cs
@@ -1,5 +1,4 @@
 using System;
-using AnyVR.Voicechat;
 using FishNet.Object;
 using FishNet.Object.Synchronizing;
 
@@ -8,9 +7,6 @@ namespace AnyVR.LobbySystem
     public class PlayerState : NetworkBehaviour
     {
         private readonly SyncVar<int> _id = new();
-        private readonly SyncVar<bool> _isAdmin = new();
-        private readonly SyncVar<bool> _isConnectedToLobby = new();
-        private readonly SyncVar<Guid> _lobbyId = new();
         private readonly SyncVar<string> _playerName = new();
 
         public int GetID()
@@ -20,14 +16,6 @@ namespace AnyVR.LobbySystem
         public string GetName()
         {
             return _playerName.Value;
-        }
-        public bool GetIsAdmin()
-        {
-            return _isAdmin.Value;
-        }
-        public Guid GetLobby()
-        {
-            return _lobbyId.Value;
         }
 
         public override void OnStartServer()
@@ -43,7 +31,6 @@ namespace AnyVR.LobbySystem
                 return;
 
             SetName(ConnectionManager.UserName);
-            VoiceChatManager.GetInstance()?.TryConnectToRoom(_lobbyId.Value, GetName(), ConnectionManager.GetInstance()!.UseSecureProtocol);
         }
 
         public void SetName(string playerName)
