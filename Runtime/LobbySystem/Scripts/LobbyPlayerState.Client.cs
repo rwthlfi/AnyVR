1f9562c2f65413e0fd5f69f46f32e053e9ec3817
Linus Witte <linus.witte@proton.me>
2025-11-21 19:49:56 +0100

    Document voicechat properties in the LobbyPlayerState

--- Runtime/LobbySystem/Scripts/LobbyPlayerState.Client.cs
+++ Runtime/LobbySystem/Scripts/LobbyPlayerState.Client.cs
@@ -13,12 +13,29 @@ namespace AnyVR.LobbySystem
         /// </summary>
         public static LobbyPlayerState Local { get; private set; }
 
+        /// <summary>
+        ///     If the player is connected to the LiveKit room of this lobby.
+        /// </summary>
         public IReadOnlyObservedVar<bool> IsConnectedToVoice => _isConnectedToVoice;
 
+        /// <summary>
+        ///     If the player's microphone is published to the LiveKit room.
+        /// </summary>
         public IReadOnlyObservedVar<bool> IsMicrophonePublished => _isMicrophonePublished;
 
+        /// <summary>
+        ///     If the player's microphone is muted.
+        /// </summary>
+        /// <remarks>
+        ///     If this is the player state of a remote player which uses the standalone build, <c>IsMicrophoneMuted</c> will always be false due to a bug in the LiveKit package.
+        ///     TODO Remove this after LiveKit fix.
+        /// <seealso href="https://github.com/livekit/client-sdk-unity/issues/152">LiveKit Issue #152</seealso>
+        /// </remarks>
         public IReadOnlyObservedVar<bool> IsMicrophoneMuted => _isMicrophoneMuted;
 
+        /// <summary>
+        ///     If the player is speaking at this moment.
+        /// </summary>
         public IReadOnlyObservedVar<bool> IsSpeaking => _isSpeaking;
 
 #endregion
