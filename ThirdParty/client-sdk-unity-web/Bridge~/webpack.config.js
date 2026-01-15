5a971a1a317a5603e712d561731c90117a552c4c
Linus Witte <linus.witte@proton.me>
2026-01-15 16:41:44 +0100

    Vendor livekit's client-sdk-unity-web package

---
+++ ThirdParty/client-sdk-unity-web/Bridge~/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    entry: {
+        'livekit': {
+            import: ['livekit-client'],
+            filename: 'Runtime/Plugins/livekit-client.jspre'
+        },
+
+        'lkbridge': {
+            import: './src/index.ts',
+            filename: 'Runtime/Plugins/livekit-jsbridge.jspre',
+            dependOn: 'livekit'
+        },
+    },
+    mode: 'production',
+    devtool: 'inline-source-map',
+    target: ["web"],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+                test: /\.jspre(\?.*)?$/i,
+                terserOptions: {
+                    keep_classnames: true,
+                    keep_fnames: true,
+                },
+            }),
+        ],
+    },
+    output: {
+        globalObject: 'this',
+        filename: '[name]',
+        path: path.resolve(__dirname, '../'),
+        library: {
+            name: '[name]',
+            type: 'window',
+        }
+    },
+};
